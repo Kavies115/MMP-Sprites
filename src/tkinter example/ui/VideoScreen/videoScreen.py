bb1d3d3678fbf04a1a31602a58586283ebb90372
kai4 <kai4@aber.ac.uk>
2023-03-12 19:17:02 +0000

    take photo (adds to the list of costumes)

--- src/tkinter example/ui/VideoScreen/videoScreen.py
+++ src/tkinter example/ui/VideoScreen/videoScreen.py
@@ -6,12 +6,15 @@ from PIL import Image as Img
 from PIL import ImageTk
 
 from components.pose_segmentor.posesegmentor import PoseSegmentor
+from global_sprite import main_sprite
+from sprite.costume import Costume
 
 
 class VideoScreen(tk.CTkFrame):
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+    segmentor = PoseSegmentor()
 
     def __init__(self, parent, controller):
         tk.CTkFrame.__init__(self, parent)
@@ -47,14 +50,19 @@ class VideoScreen(tk.CTkFrame):
 
         button_home_screen.pack(padx=6, pady=24, side=tk.LEFT)
 
+        button_take_photo = tk.CTkButton(frame_for_video, text="Take Photo", font=("Berlin Sans FB", 56),
+                                         command=lambda: self._take_photo(), height=100, width=500)
+
+        button_take_photo.pack(padx=8, pady=8, side=tk.BOTTOM)
+
         self.show_frames()
 
     def show_frames(self):
         # Capture the video frame by frame
         _, frame = self.cap.read()
 
-        # Segment the frame (when this part of the )
-        segmented_frame = PoseSegmentor().image_segmentor(frame)
+        # Segment the frame
+        segmented_frame = self.segmentor.image_segmentor(image=frame)
 
         # Convert image from one color space to other
         opencv_image = cv2.cvtColor(segmented_frame, cv2.COLOR_BGR2RGBA)
@@ -72,3 +80,11 @@ class VideoScreen(tk.CTkFrame):
         label_widget.configure(image=photo_image)
 
         label_widget.after(10, self.show_frames)
+
+    def _take_photo(self):
+        # For testing just take the img from the camera
+        _, frame = self.cap.read()
+
+        costume = Costume(frame)
+
+        main_sprite.add_list_img(costume)
